05aa62eadef97180eb740e427325d01c217f53cd
Jairam Venkit <jairam0@gmail.com>
2024-03-14 19:44:29 -0700

    Added reduce functionality

--- mapreduce/reduce.go
+++ mapreduce/reduce.go
@@ -1,6 +1,24 @@
 package mapreduce
 
 // Interface defining the structure of "reduce" function
-type Reduce interface {
+type Reducer interface {
 	Reduce(key string, values []string) string
+}
+
+// struct used to implement the "reduce()" function
+type reduceWorker struct {
+	reducer Reducer
+}
+
+// Generate a new object of type reduceWorker
+func newReduceWorker(reducer Reducer) *reduceWorker {
+	return &reduceWorker{
+		reducer: reducer,
+	}
+}
+
+// Perfoms the reduce operation.
+// The client is expected to implement their own "reduce" function for their use case
+func (worker *reduceWorker) performReduce(key string, values []string) string {
+	return worker.reducer.Reduce(key, values)
 }
