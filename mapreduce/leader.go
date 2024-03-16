e258ea6ba1f8b5519bf264f7157d26988f8a6fe6
Jairam Venkit <jairam0@gmail.com>
2024-03-15 18:10:39 -0700

    Added sequential execution of MR to leader

--- mapreduce/leader.go
+++ mapreduce/leader.go
@@ -1,29 +1,29 @@
 package mapreduce
 
-import "sync"
+import (
+	"net"
+	"sync"
+)
 
 /*
-Each MapReduce operation must obey the following constructs:
+Leader's duties:
 
-Information stored by the leader node:
-
-Node Identity:
-1. Identity of the follower machine (for non-idle tasks)
-
-Job Identity:
-1. State of the job
-
-MapReduce operation structure
+1. Split a task into "M" map and "R" reduce tasks
+2. Schedule a task to a particular node
+3. Monitor status of the task
+4. Frequently poll and obtain the status of a task
+	* Design an HTTP Server for this
 */
 
 type Leader struct {
-	address         string      // Address to listen on for follower registration and communication.
-	mapTasks        []string    // Filenames of the input files to be processed.
-	reduceTasks     int         // The number of reduce tasks. Mentioned as "R" in the MapReduce paper.
-	followers       []string    // Registered follower addresses.
-	followerChannel chan string // Channel for receiving available followers.
-	tasksDone       chan bool   // Channel to signal task completion.
-	lock            sync.Mutex  // Mutex to protect shared access to Leader's state.
+	address         string       // Address to listen on for follower registration and communication.
+	mapTasks        []string     // Filenames of the input files to be processed.
+	reduceTasks     int          // The number of reduce tasks. Mentioned as "R" in the MapReduce paper.
+	followers       []string     // Registered follower addresses.
+	followerChannel chan string  // Channel for receiving available followers.
+	tasksDone       chan bool    // Channel to signal task completion.
+	lock            sync.Mutex   // Mutex to protect shared access to Leader's state.
+	listener        net.Listener // Listener object for RPC communications
 }
 
 // Returns a new Leader node object
@@ -44,4 +44,22 @@ func (leader *Leader) registerNewFollower(followerAddress string) {
 	defer leader.lock.Unlock()
 
 	leader.followers = append(leader.followers, followerAddress)
+}
+
+func (leader *Leader) sequentialExecution(jobName string, files []string, numReduce int,
+	mapFunction Mapper, reduceFunction Reducer) {
+	leader = newLeader("leader", files, numReduce)
+	mapWorker := newMapWorker(mapFunction)
+	// reduceWorker := newReduceWorker(reduceFunction)
+
+	// sequentially run all the map tasks
+	for _, _ = range leader.mapTasks {
+		// @TODO: FINISH THIS PORTION PROPERLY
+		mapWorker.performMap("key", "val")
+	}
+
+	// sequentially running the reduce operations
+	for i := 0; i < numReduce; i++ {
+		// reduceWorker.performReduce("key", "value")
+	}
 }
